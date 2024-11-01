1026cd4abf3171315d941398f6dcb44c490d350f
Leoo2002 <leonelquirogapacheco@gmail.com>
2024-11-01 16:28:17 -0300

    solucionamos error con + en nombre de imagenes

--- src/app/Castillo.service.ts
+++ src/app/Castillo.service.ts
@@ -9,15 +9,16 @@ export class CastilloService{
     productos:Producto[]=[
         new Producto(["/img/chico/castillochico.jpeg","/img/castillochico2.jpeg","/img/castillochico3.jpeg"],"Castillo Chico","dimenciones"),
         new Producto(["/img/acuatico/acuatico.jpeg"],"Castillo Acuatico","dimenciones"),
-        new Producto(["/img/conejo/conejo.jpeg","/img/castillochico2.jpeg","/img/castillochico3.jpeg"],"Castillo Bugs Bunny","dimenciones"),
-        new Producto(["/img/lorenzo/lorenzo3.jpeg","/img/castillochico2.jpeg","/img/castillochico3.jpeg"],"Castillo Cachorrito","dimenciones")
+        new Producto(["/img/conejo/conejo.jpeg",],"Castillo Bugs Bunny","dimenciones"),
+        new Producto(["/img/lorenzo/lorenzo3.jpeg",],"Castillo Cachorrito","dimenciones"),
+        new Producto(["/img/lapiz/lapiz.jpeg",],"Castillo Lapiz","dimenciones")
     ];
     combos:Producto[]=[
       new Producto(["/img/atril/atriles2.jpeg"],"Atriles Para Pintar","dimenciones"),
       new Producto(["/img/metegol/metegol.jpeg"],"Metegol","dimenciones"),
-      new Producto(["/img/combos/lorenzo+metegol.jpeg"],"Castillo + Metegol","dimenciones"),
-      new Producto(["/img/combos/castillochico+atril.jpeg"],"Castillo + Atril","dimenciones"),
-      new Producto(["/img/combos/lorenzo+metegol+atril.jpeg"],"Castillo + Atril + Metegol","dimenciones"),
+      new Producto(["/img/combos/lorenzo-metegol.jpeg"],"Castillo + Metegol","dimenciones"),
+      new Producto(["/img/combos/castillochico-atril.jpeg"],"Castillo + Atril","dimenciones"),
+      new Producto(["/img/combos/lorenzo-metegol-atril.jpeg"],"Castillo + Atril + Metegol","dimenciones"),
       new Producto(["/img/combos/3castillos.jpeg"],"Combo Castillos Varios","dimenciones")
     ];
 }
